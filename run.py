3d24b1d42b0c47a266fee146c6e0660f1da95d10
LuQuintas11 <luciaquintas14@gmail.com>
2022-05-21 19:34:39 +0000

    Add validation to check foe exactly 6 values in provided

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-import gspread
+import gspread 
 from google.oauth2.service_account import Credentials
 
 SCOPE = [
@@ -7,13 +7,37 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
     ]
 
-CREDS = Credentials.from_service_account_file("creds.json")
+CREDS = Credentials.from_service_account_file('cred.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open("love-sandwiches")
+SHEET = GSPREAD_CLIENT.open('love-sandwiches')
 
-sales = SHEET.worksheet("sales")
 
-data = sales.get_all_values()
+def get_sales_data():
+    """
+    Get sales figures input from the user.
+    """
+    print("Please enter sales data from the last market.")
+    print("Data should be six numbers, separated by commas.")
+    print("Example: 10,20,30,40,50,60\n")
 
-print(data)
+    data_str = input("Enter your data here: ")
+  
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+
+
+def validate_data(values):
+    """
+    Inside the try, converts all string values into integers
+    """
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+
+get_sales_data()
+
